3b2378d4a5533a56ff0290851dd1cb1222215751
jamesd <jamesbluffin86@gmail.com>
2019-08-20 09:15:38 -0500

    uploading

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <MainNav/>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+
+import MainNav from "./components/MainNav.vue"
+
+</script>
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
---
+++ src/components/MainNav.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hello">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainNav'
+}
+</script>
